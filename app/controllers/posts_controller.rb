ab0702a5c5e14828cb28c40a5f2633ac024ef121
koan <sylvain.kraisin@gmail.com>
2015-10-06 14:13:58 +0200

    first commit blog

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,8 @@ class PostsController < ApplicationController
   	@posts = Post.all
   end
   def show
+    @posts = Post.all
   	@post = Post.find(params[:id])
   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150929121858) do
+ActiveRecord::Schema.define(version: 20151005100317) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string  "author"
+    t.text    "content"
+    t.integer "post_id"
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
