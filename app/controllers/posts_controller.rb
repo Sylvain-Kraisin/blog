856cdd755bb62a7d67a9c60b5039722269e7a6aa
koan <sylvain.kraisin@gmail.com>
2015-10-08 14:15:56 +0200

    gem kaminari pagination added + commentaires ameliorés

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,13 @@
 class PostsController < ApplicationController
   def index
-  	@posts = Post.all
+  	@posts = Post.page(params[:page]).per(5)
   end
   def show
     @posts = Post.all
   	@post = Post.find(params[:id])
   end
-
+  def create
+    Post.create title: params[:title], content: params[:content]
+    redirect_to "/posts"
+  end
 end
